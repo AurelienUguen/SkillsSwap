39407a097a39b7e64895137b6e893cf8faa46562
DorianRONDI <dorianrondi@gmail.com>
2023-07-03 10:39:45 +0200

    install api

--- apps/api/config/bundles.php
+++ apps/api/config/bundles.php
@@ -12,4 +12,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
