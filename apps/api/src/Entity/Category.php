7f753f1b02fd363236ee9018cb8b0caf68799342
DorianRONDI <dorianrondi@gmail.com>
2023-07-03 09:58:51 +0200

    Creation des Entities

---
+++ apps/api/src/Entity/Category.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\OneToOne(mappedBy: 'category_id', cascade: ['persist', 'remove'])]
+    private ?Subject $subject = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSubject(): ?Subject
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(Subject $subject): static
+    {
+        // set the owning side of the relation if necessary
+        if ($subject->getCategoryId() !== $this) {
+            $subject->setCategoryId($this);
+        }
+
+        $this->subject = $subject;
+
+        return $this;
+    }
+}
---
+++ apps/api/src/Entity/Lesson.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LessonRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LessonRepository::class)]
+class Lesson
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column]
+    private ?bool $irl = null;
+
+    #[ORM\Column]
+    private ?bool $visio = null;
+
+    #[ORM\ManyToOne(inversedBy: 'lessons')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Course $course_id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'lessons')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user_id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function isIrl(): ?bool
+    {
+        return $this->irl;
+    }
+
+    public function setIrl(bool $irl): static
+    {
+        $this->irl = $irl;
+
+        return $this;
+    }
+
+    public function isVisio(): ?bool
+    {
+        return $this->visio;
+    }
+
+    public function setVisio(bool $visio): static
+    {
+        $this->visio = $visio;
+
+        return $this;
+    }
+
+    public function getCourseId(): ?Course
+    {
+        return $this->course_id;
+    }
+
+    public function setCourseId(?Course $course_id): static
+    {
+        $this->course_id = $course_id;
+
+        return $this;
+    }
+
+    public function getUserId(): ?User
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(?User $user_id): static
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+}
