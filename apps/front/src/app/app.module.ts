e6275bdef0637aeb6e6075b903a9d7d76ce43013
aurelienuguen <aurelien.uguen@gmail.com>
2023-07-05 11:09:34 +0200

    get api data on angular

--- apps/front/src/app/app.module.ts
+++ apps/front/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './components/categories/categories.component'
+
+
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
