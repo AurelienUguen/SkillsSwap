41e5eb9c79a81c31e410b2e67e1fb91962a06eba
DorianRONDI <dorianrondi@gmail.com>
2023-07-13 21:02:18 +0200

    Nouvelle logique de routing + ajout read_sheet de propiété name dans Entity.Category.php

--- apps/front/src/app/app-routing.module.ts
+++ apps/front/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { SheetDetailComponent } from './components/sheet-detail/sheet-detail.com
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home-components/home/home.component';
 import { CategoriesComponent } from './components/categories/categories.component';
+import { SheetComponent } from './components/sheet/sheet.component';
 
 
 
   const routes: Routes = [
     { path: '', component: HomeComponent},
+    { path: 'sheet', component: SheetComponent },
     { path: ':sheet', component: SheetDetailComponent }
     { path: 'categories', component: CategoriesComponent },
     { path: 'login', component: LoginComponent },
