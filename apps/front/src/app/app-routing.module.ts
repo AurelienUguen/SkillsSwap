3d4d834bec59105d9607363b4296b22a48ecc661
DorianRONDI <dorianrondi@gmail.com>
2023-07-13 17:03:22 +0200

    SkillSwap is SkillSwagged

--- apps/front/src/app/app-routing.module.ts
+++ apps/front/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { SheetDetailComponent } from './components/sheet-detail/sheet-detail.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home-components/home/home.component';
+import { CategoriesComponent } from './components/categories/categories.component';
 
 
 
   const routes: Routes = [
     { path: '', component: HomeComponent},
-    { path: ':sheet', component: SheetDetailComponent },
+    { path: ':sheet', component: SheetDetailComponent }
+    { path: 'categories', component: CategoriesComponent },
     { path: 'login', component: LoginComponent },
 ];
 
