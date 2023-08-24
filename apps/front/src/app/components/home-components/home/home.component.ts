c1c08c25bc90a86119d34157f881e0d2a92a045e
DorianRONDI <dorianrondi@gmail.com>
2023-08-24 16:52:21 +0200

    Système de redirection après achat de Tokkens

--- apps/front/src/app/components/home-components/home/home.component.ts
+++ apps/front/src/app/components/home-components/home/home.component.ts
@@ -1,20 +1,8 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-/* 
-  public prenom: string;
-  public nom: string;
- */
-  constructor(private route: ActivatedRoute) {
-    /* 
-    this.prenom = this.route.snapshot.paramMap.get('prenom');
-    this.nom = this.route.snapshot.paramMap.get('nom');
-   */
-  }
-}
+export class HomeComponent{}
