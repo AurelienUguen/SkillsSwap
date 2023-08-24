4e27ff79799f4fd3bf16d3d0abc45deee5bb844c
DorianRONDI <dorianrondi@gmail.com>
2023-08-24 15:31:15 +0200

    rien

--- apps/front/src/app/components/home-components/home/home.component.ts
+++ apps/front/src/app/components/home-components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+/* 
+  public prenom: string;
+  public nom: string;
+ */
+  constructor(private route: ActivatedRoute) {
+    /* 
+    this.prenom = this.route.snapshot.paramMap.get('prenom');
+    this.nom = this.route.snapshot.paramMap.get('nom');
+   */
+  }
 }
