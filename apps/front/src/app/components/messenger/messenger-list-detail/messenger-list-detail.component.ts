00f8a2e305ba6fe3adf378ed0ae79d23af903f28
Kuba Olszak <analogband@gmail.com>
2023-08-30 09:23:32 +0200

    Slug de l'owner des message enlevé

--- apps/front/src/app/components/messenger/messenger-list-detail/messenger-list-detail.component.ts
+++ apps/front/src/app/components/messenger/messenger-list-detail/messenger-list-detail.component.ts
@@ -57,11 +57,11 @@ export class MessengerListDetailComponent implements OnInit{
   }
 
   getConversationsByUser(participants: []) {
-    // console.log(participants);
     participants.forEach((el: any) => {
-      if(!(el['user']['slug'] === this.slug || el['user']['slug'] === undefined)) {
-        this.participantFirstname = el['user']['slug'].split("-")[0];
-        this.participantLastname = el['user']['slug'].split("-")[1];
+      // console.log(el['user'].slice(11).split("-")[0]);
+      if(!(el['user'].slice(11) === this.slug || el['user'].slice(11) === undefined)) {
+        this.participantFirstname = el['user'].slice(11).split("-")[0];
+        this.participantLastname = el['user'].slice(11).split("-")[1];
         // this.participantFirstname = el['user']['firstname'];
         // this.participantLastname = el['user']['lastname'];
       }
